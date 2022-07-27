f44f8fd779d419c37b907ea89b11aecb408d598f
Gosha Gaponov <egoriks1994@gmail.com>
2022-07-28 00:35:54 +0300

    8 block 006 Feat: add validation and change uncontrolled component to controlled component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,12 +2,23 @@ import React, {useState} from 'react';
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
+
 const AddUser = () => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
     const addUserHandler = (e) => {
         e.preventDefault();
-        console.log(enteredUsername, enteredAge)
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            console.log('Empty')
+            return;
+        }
+        if (+enteredAge < 1){
+            console.log('Age must be > 0')
+            return;
+        }
+        console.log(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     }
     const usernameChangeHandler = (e) => {
         setEnteredUsername(e.target.value);
@@ -19,9 +30,9 @@ const AddUser = () => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username </label>
-                <input id="username" type="text" onChange={usernameChangeHandler}/>
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
                 <label htmlFor='age'>Age (Years) </label>
-                <input id="age" type="number" onChange={ageChangeHandler}/>
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
                 <Button type='submit'>Add user</Button>
             </form>
         </Card>
